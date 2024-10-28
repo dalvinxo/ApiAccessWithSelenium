92a1066eed01123080e58641c1835f7fd2b60752
dalvinxo <dalvingamer@gmail.com>
2024-10-28 15:00:30 -0400

    feat: pruebas unitarias terminadas con selenium

--- Selenium.Test/Tests/WebTests.cs
+++ Selenium.Test/Tests/WebTests.cs
@@ -7,21 +7,21 @@ using System;
 namespace Selenium.Test.Tests
 {
     [TestFixture]
-    public class WebTests: IDisposable
+    public class WebTests : IDisposable
     {
-        String test_url = "http://localhost:5000";
+        // String test_url = "http://localhost:5000";
+        String test_url = "http://localhost:5070/Post/Create/517";
         private ChromeDriver _driver;
         private WebDriverWait _wait;
 
         [SetUp]
         public void Setup()
         {
-            
+
             var options = new ChromeOptions();
-            options.AddArgument("--headless"); 
+            // options.AddArgument("--headless");
             _driver = new ChromeDriver(options);
-            
-            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
         }
 
         [Test]
@@ -31,8 +31,70 @@ namespace Selenium.Test.Tests
             _driver.Navigate().GoToUrl(test_url);
 
             string pageTitle = _driver.Title;
-            Assert.That(pageTitle, Is.EqualTo("Home Page - Selenium Web"));
+            Assert.That(pageTitle, Is.EqualTo("Crear Artículo - Games PC"));
         }
+
+        [Test]
+        public void Test_FillAndSubmitForm()
+        {
+            _driver.Navigate().GoToUrl(test_url);
+
+            // Esperar a que el formulario esté listo
+            _wait.Until(driver => driver.FindElement(By.CssSelector("form")));
+
+            // Rellenar el campo 'Name'
+            var nameField = _driver.FindElement(By.Id("Name"));
+            nameField.Clear();
+            nameField.SendKeys("Nombre del Usuario");
+
+            // Seleccionar el género 'Hombre'
+            var genreMaleOption = _driver.FindElement(By.CssSelector("input[value='Hombre']"));
+            genreMaleOption.Click();
+
+            // Rellenar el campo 'Age'
+            var ageField = _driver.FindElement(By.Id("Age"));
+            ageField.Clear();
+            ageField.SendKeys("25");
+
+            // Seleccionar 'País' del menú desplegable
+            var paisDropdown = new SelectElement(_driver.FindElement(By.Id("PaisId")));
+            paisDropdown.SelectByValue("1"); // Cambiar a un valor válido en tu lista de países
+
+            var titleField = _driver.FindElement(By.Id("Title"));
+            titleField.Clear();
+            titleField.SendKeys("Título del Artículo");
+
+            var descriptionField = _driver.FindElement(By.Id("Description"));
+            descriptionField.Clear();
+            descriptionField.SendKeys("Descripción del artículo de prueba.");
+
+            // var rateField = _driver.FindElement(By.Id("Rate"));
+            // rateField.SendKeys("7");
+
+            // var rateField = _driver.FindElement(By.Id("Rate"));
+            // rateField.Clear();
+            // rateField.SendKeys("6");
+
+            var rateField = _driver.FindElement(By.Id("rate-input"));
+            rateField.Clear();
+            rateField.SendKeys("7");
+
+            // var submitButton = _driver.FindElement(By.CssSelector("button[type='submit']"));
+            // submitButton.Click();
+
+            // var submitButton = _driver.FindElement(By.CssSelector("button[type='submit']"));
+            // ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].scrollIntoView(true);", submitButton);
+            // submitButton.Click();
+
+            var submitButton = _driver.FindElement(By.CssSelector("button[type='submit']"));
+            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].click();", submitButton);
+
+
+            // Validar que la redirección o resultado esperado ha ocurrido
+            Assert.That(_driver.Url, Is.EqualTo("http://localhost:5070/Post")); // Cambia esta URL según corresponda
+        }
+
+
 
         // [TearDown]
         // public void TearDown()
@@ -48,9 +110,9 @@ namespace Selenium.Test.Tests
             Dispose();
         }
 
-         public void Dispose()
+        public void Dispose()
         {
-            
+
             _driver?.Dispose();
         }
     }
