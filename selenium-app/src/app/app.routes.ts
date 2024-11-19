acd1a92b5a8a83584d8f2b6a3e3cb358c38c1935
dalvinxo <dalvingamer@gmail.com>
2024-11-18 20:29:13 -0400

    feat: se elimino la anterior app de angular, para crear esta en una version mas vieja

--- selenium-app/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { JuegosComponent } from './juegos/juegos.component';
-import { PostComponent } from './post/post.component';
-
-export const routes: Routes = [
-  { path: 'juegos', component: JuegosComponent },
-  { path: 'post', component: PostComponent },
-  { path: '', redirectTo: '/juegos', pathMatch: 'full' },
-];
